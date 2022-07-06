a99274dda0d74829b83517c06452fca6173c15e5
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-06 13:59:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -301,6 +301,6 @@ app.use((err, req, res, next) => {
 
 // listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
