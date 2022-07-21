f243ef57f6ebee804fe1fe65878c2ab93ffaf2db
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-21 14:43:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ let allowedOrigins =
 
 app.use(cors({
   origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+          let message = 'The CORS policy for this application doesn\’t allow access from origin ' + origin;
+          return callback(new Error(message), false);
+      }
+      return callback(null, true);
   }
 }));
 
