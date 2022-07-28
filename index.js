589bdae5208a22c1411d87d014c4da7423838ef4
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-28 14:14:29 +0200

    troubleshooting CORS access

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ app.use((err, req, res, next) => {
 
 
 // listen for requests
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 1234;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
