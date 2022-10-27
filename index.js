51a27e1ac50ae1ba877a162d815167b82ad9fa24
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-10-27 17:33:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,19 +46,19 @@ let allowedOrigins = [
   'https://movie-info-online.herokuapp.com'
 ];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       // If a specific origin isn't found on the list of allowed origins
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     }
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      // If a specific origin isn't found on the list of allowed origins
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 let auth = require('./auth')(app);
 
