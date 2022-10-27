50f0735fe7317842fe9bcbc7c1e6fcd75b925b95
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-10-27 17:37:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,30 +35,30 @@ app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+// app.use(cors());
 
 // // To specify particular URI
 
-let allowedOrigins = [
-  'http://localhost:8080',
-  'http://testsite.com',
-  'http://localhost:1234',
-  'https://movie-info-online.herokuapp.com'
-];
+// let allowedOrigins = [
+//   'http://localhost:8080',
+//   'http://testsite.com',
+//   'http://localhost:1234',
+//   'https://movie-info-online.herokuapp.com'
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      // If a specific origin isn't found on the list of allowed origins
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       // If a specific origin isn't found on the list of allowed origins
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 
 let auth = require('./auth')(app);
 
