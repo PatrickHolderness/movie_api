34497fed8fb3b579bd95d2ab4025a8f53da7e577
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-10-27 18:40:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,27 +38,27 @@ app.use(cors());
 
 // To specify particular URI
 
-let allowedOrigins = [
-  'http://localhost:8080',
-  'http://testsite.com',
-  'http://localhost:1234',
-  'https://movie-info-online.herokuapp.com',
-  'patrickholderness.github.io'
-];
+// let allowedOrigins = [
+//   'http://localhost:8080',
+//   'http://testsite.com',
+//   'http://localhost:1234',
+//   'https://movie-info-online.herokuapp.com',
+//   'patrickholderness.github.io'
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      // If a specific origin isn't found on the list of allowed origins
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       // If a specific origin isn't found on the list of allowed origins
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     }
+//   })
+// );
 
 // app.options('*', cors());
 // var allowCrossDomain = function(req, res, next) {
