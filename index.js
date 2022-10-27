12412f955655cc7b5b1e82daa24ff79cf6310961
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-10-27 21:08:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,13 @@ app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-
+app.use(cors())
+;
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 // // To specify particular URI
 
 // let allowedOrigins = [
